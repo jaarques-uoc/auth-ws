f9d7f02c9651bb15a8b1396f836ffe724219ba96
Jordi <jaarques@uoc.edu>
2019-05-25 23:56:06 +0200

    Initial commit

---
+++ src/main/java/com/jaarquesuoc/shop/authws/AuthWsApplication.java
@@ -0,0 +1,13 @@
+package com.jaarquesuoc.shop.authws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaarquesuoc/shop/authws/AuthWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jaarquesuoc.shop.authws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
